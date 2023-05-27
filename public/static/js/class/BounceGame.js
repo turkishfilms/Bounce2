3a2781df71fdb37819bcf5a5a0a56ba28ce7361a
Reginald Robinson <ddrbnsn@gmail.com>
2023-05-27 12:19:03 -0700

    tidied bouncegame

--- public/static/js/class/BounceGame.js
+++ public/static/js/class/BounceGame.js
@@ -6,7 +6,8 @@ class BounceGame {
         boundingBox = new BoundingBox(),
         scoreManager = new ScoreManager(),
         physics = new Physics(),
-        domManager = new DOMManager() } = {}) {
+        domManager = new DOMManager()
+    } = {}) {
         this.balls = balls
         this.paddles = paddles
 
@@ -17,38 +18,50 @@ class BounceGame {
 
         this.name = name || games.length + 1
 
-        this.#addPaddle(this.#newPaddle())
-        this.#addBall(this.#newBall())
+        this.#_addPaddle(this.#_newPaddle())
+        this.#_addBall(this.#_newBall())
         this.domManager.addDiv(`Game ${this.name} score`, this.scoreManager.currentScore())
         this.domManager.addDiv(`Game ${this.name} highscore`, this.scoreManager.currentHighscore())
     }
 
     nextFrame() {
-        this.#ballsNext()
-        this.#paddlesNext()
-        this.domManager.updateScoreDisplay(this.name, this.scoreManager.currentScore(), this.scoreManager.currentHighscore())
+        this.#_ballsNext()
+        this.#_paddlesNext()
+        this.domManager.updateScoreDisplay(
+            this.name,
+            this.scoreManager.currentScore(),
+            this.scoreManager.currentHighscore()
+        )
     }
 
     receiveBall(pos) {
-        if (this.#contains(pos)) this.#addBall(this.#newBall())
+        if (this.#_contains(pos)) this.#_addBall(this.#_newBall())
     }
 
-    //#private
+    //#_private
 
-    #addPaddle(paddle) { this.paddles.push(paddle) }
-    #addBall(ball) { this.balls.push(ball) }
+    #_addPaddle(paddle) {
+        this.paddles.push(paddle)
+    }
 
-    #newPaddle() {
+    #_addBall(ball) {
+        this.balls.push(ball)
+    }
+
+    #_newPaddle() {
         const x = this.boundingBox.maxX / 2
         const w = this.boundingBox.maxX / 4
         const h = w / 4
         const y = (0.90 * this.boundingBox.maxY) - h
         return new Paddle({
-            x: x, w: w, h: h, y: y
+            x: x,
+            w: w,
+            h: h,
+            y: y
         })
     }
 
-    #newBall() {
+    #_newBall() {
         return new Ball({
             xspeed: random(-5, 5),
             yspeed: random(-5, 0),
@@ -59,54 +72,42 @@ class BounceGame {
         })
     }
 
-    #paddlesNext() {
+    #_paddlesNext() {
         this.paddles.forEach(p => {
             p.x = mouseX - p.w / 2
             p.show(Fimg)
         })
     }
 
-    #ballsNext() {
+    #_ballsNext() {
         this.balls.forEach((b, i) => {
             b.move(0)
             b.show(CJimg)
-            if (this.#isBallOffscreen(b)) this.#ballFell(i)
+            if (this.#_isBallOffscreen(b)) this.#_ballFell(i)
         })
     }
 
-    #ballFell(index) {
-        this.#removeBall(index)
+    #_ballFell(index) {
+        this.#_removeBall(index)
         this.scoreManager.resetScore()
-        this.#addBall(this.#newBall())
+        this.#_addBall(this.#_newBall())
     }
 
-    #removeBall(ballIndex) { this.balls.splice(ballIndex, 1) }
-
-
-    //
-    //DOM
-
-    #addDiv = (id) => { //FIXME
-        const scoreDiv = document.createElement("p");
-        scoreDiv.id = id
-        scoreDiv.appendChild(document.createTextNode(`0\n`))
-        document.body.prepend(scoreDiv)
-    }
-
-    #updateScoreDisplay() {
-        document.getElementById(`Game ${this.name} score`).textContent = `Points: ${this.scoreManager.currentScore()}\n`
-        document.getElementById(`Game ${this.name} highscore`).textContent = `Highscore: ${this.scoreManager.currentHighscore()}\n`
+    #_removeBall(ballIndex) {
+        this.balls.splice(ballIndex, 1)
     }
 
     //conditional
 
-    #isBallOffscreen(ball) {
+    #_isBallOffscreen(ball) {
         return ball.y >= this.boundingBox.maxY
     }
 
-    #contains(pos) {
-        return pos.x > this.boundingBox.minX && pos.x < this.boundingBox.maxX && pos.y > this.boundingBox.minY && pos.y < this.boundingBox.maxY
+    #_contains(pos) {
+        return (
+            pos.x > this.boundingBox.minX &&
+            pos.x < this.boundingBox.maxX &&
+            pos.y > this.boundingBox.minY &&
+            pos.y < this.boundingBox.maxY)
     }
-
-
 }
